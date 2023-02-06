87d054a6c84e5417a3374e4e1e0374a7618064b3
michaelcoulentianos <michael.coulentianos@sanlamsky.co.za>
2023-02-06 12:48:21 +0200

    AddTwoNumbersLinkListsTests

--- UnitTests/AddTwoNumbersLinkListsTests.cs
+++ UnitTests/AddTwoNumbersLinkListsTests.cs
@@ -3,10 +3,21 @@ namespace tests;
 public class AddTwoNumbersLinkListsTests
 {
     [TestCase(ExpectedResult = true)]
-    public bool Simple_Test()
+    public bool Simple()
     {
         ListNode l1 = new(2, new(4, new(3)));
         ListNode l2 = new(5, new(6, new(4)));
+
+       var result = AddTwoNumbersLinkLists.AddTwoNumbers(l1, l2);
+
+        return true;
+    }
+
+[TestCase(ExpectedResult = true)]
+     public bool Zero()
+    {
+        ListNode l1 = new(0);
+        ListNode l2 = new(0);
 
        var result = AddTwoNumbersLinkLists.AddTwoNumbers(l1, l2);
 
